e78c78454949baf2a5635b4c58137ef3de4d6770
rich <xiegw@xiaolajiao.com>
2016-08-24 16:00:45 +0800

    language modify

--- xljAdmin/settings.py
+++ xljAdmin/settings.py
@@ -113,7 +113,7 @@ TEMPLATE_LOADERS = (
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
+    # 'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
